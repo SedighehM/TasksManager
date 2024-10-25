91882a931549a7c0d9967ce733ca01d63d48d562
SedighehM <semohammadkhani@gmail.com>
2024-10-25 17:26:22 -0600

    Navbar was implemented

--- client/src/redux/slices/authSlice.js
+++ client/src/redux/slices/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   user: localStorage.getItem("userInfo")
     ? JSON.parse(localStorage.getItem("userInfo"))
-    : 'sddf',
+    : { name: "Sedigheh Mohammadkhani" },
 
   isSidebarOpen: false,
 };
